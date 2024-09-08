7c6672441d9567f0c792286a5ac5fc05f7084615
Kelly Popko <kelpopko@gmail.com>
2024-09-07 20:49:55 -0500

    Iteratation 1 of anagram (ruby)
    Initializes target word downcased and as readable attribute
    Helper methods check if the target and candiate are the same, and if the
    sorted characters are the same.
    
    `match` selects non idential words with the same characters

---
+++ anagram.rb
@@ -0,0 +1,27 @@
+class Anagram
+
+  private
+
+  def initialize(target_word)
+    @target_word = target_word.downcase
+  end
+
+  def same_letters?(word)
+    word.downcase.chars.sort == target_word.chars.sort
+  end
+
+  def non_indentical?(word)
+    word.downcase != target_word
+  end
+
+  public
+
+  attr_reader :target_word
+
+  def match(candidates)
+    candidates.select do |candidate|
+      non_indentical?(candidate) and same_letters?(candidate)
+    end
+  end
+
+end
